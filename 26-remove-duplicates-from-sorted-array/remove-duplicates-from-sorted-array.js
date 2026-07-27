86799359240edb6b6e5c058b54f8edc8db36fb0d
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-07-27 10:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (98.89%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+let j = 0;
+
+for(let i = 0 ; i < nums.length ; i ++){
+  if(nums[i] > nums[j]){
+    nums[j+1] = nums[i];
+    j = j +1;
+
+  }
+ 
+}
+ return j+1
+
+};
