6499352c8768e2d6fea1e2ea8a4daa11346e44ba
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-07 10:46:11 +0530

    Time: 1 ms (83.99%) | Memory: 58.6 MB (34.11%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    
+    let newArr  = [];
+
+    for(let i = 0 ; i < words.length ; i++){
+        for(let j = 0 ; j < words[i].length ; j++){
+            if(words[i][j] === x){
+            newArr.push(i);
+            break;
+            }
+        }
+    }
+    return newArr;
+};
