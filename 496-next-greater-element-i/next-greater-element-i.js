3bb982c37ad5f1b38361f56f95d00fb73c2257ca
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-12 23:31:26 +0530

    Time: 3 ms (67.94%) | Memory: 58.6 MB (15.60%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let stack = [];
+    let map = new Map();
+
+    for(let i = nums2.length - 1 ; i >= 0 ; i--){
+        while(stack.length !== 0 && stack[stack.length - 1] < nums2[i]){
+            stack.pop()
+        }
+        if(stack.length === 0 ){
+            map.set(nums2[i] , -1)
+        }
+        else{
+            map.set(nums2[i] ,stack[stack.length -1])
+        }
+        stack.push(nums2[i]);
+    }
+    let ans = [];
+    for(let i = 0 ; i < nums1.length ; i++){
+       ans.push(map.get(nums1[i]));
+    }
+    return ans;
+};
