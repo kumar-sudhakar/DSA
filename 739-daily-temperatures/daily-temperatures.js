cdf5507d01e621886cd76dd51feed810e877a72e
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-13 09:48:45 +0530

    Time: 126 ms (5.08%) | Memory: 80.2 MB (31.67%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let stack = [];
+    let answer = new Array(temperatures.length).fill(0);
+
+    for(let i = temperatures.length - 1 ; i >= 0 ; i--){
+        while(stack.length !== 0 && temperatures[stack[stack.length-1]] <= temperatures[i]){
+            stack.pop();
+        }
+        if(stack.length !== 0){
+            answer[i] = ([stack[stack.length-1]] - i);
+        }
+        stack.push(i);
+    }
+    return answer;
+};
