0945380ec8e4b4def82d8cb009d94d4c78998377
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-15 17:15:28 +0530

    Time: 0 ms (100.00%) | Memory: 64.8 MB (31.61%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    let left = 0;
+    let right = arr.length-1;
+
+    while(left < right){
+        let mid = Math.floor((left+right)-1);
+        if(arr[mid] < arr[mid+1]){
+            left = mid + 1;
+        }
+        else{
+            right = mid;
+        }
+    }
+    return right;
+};
