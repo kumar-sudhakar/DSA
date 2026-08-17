503285d39d597aaba21626570f4aebb81cb1dc6c
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-17 13:01:32 +0530

    Time: 3 ms (48.83%) | Memory: 63.8 MB (7.26%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length-1;
+    let max = 0;
+
+    while(left < right){
+        let width = right - left;
+        let h = Math.min(height[left] , height[right]);
+
+        let area = width * h;
+        max = Math.max(max , area);
+        if(height[left] < height[right]){
+            left++;
+        } else{
+            right--;
+        }
+    }
+    return max;
+};
