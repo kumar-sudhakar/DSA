0f99a2e05627e48491fea6c12987940951e711f3
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-22 21:54:03 +0530

    Time: 54 ms (59.07%) | Memory: 59 MB (57.69%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -13,13 +13,16 @@
 var hasCycle = function(head) {
     let slow = head;
     let fast = head;
-    while(fast !== null && fast.next !== null  ){
+
+    while(fast !== null && fast.next !== null){
         slow = slow.next;
         fast = fast.next.next;
-        if(slow === fast){
+
+        if(fast === slow){
             return true;
         }
-        
+    
     }
     return false;
+    
 };
