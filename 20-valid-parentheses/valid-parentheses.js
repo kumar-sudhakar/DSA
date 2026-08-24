5955e65ae65700bf41b8ba9d1a7c5ea8009af2c1
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-24 11:47:52 +0530

    Time: 4 ms (51.38%) | Memory: 54.1 MB (94.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -2,36 +2,23 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-
+var isValid = function (s) {
     let stack = [];
-
     for (let i = 0; i < s.length; i++) {
-
-        if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
-            stack.push(s[i]);
-        } 
+        if ("[({".includes(s[i])) {
+            stack.push(s[i])
+        }
         else {
-
-            if (stack.length === 0) {
-                return false;
-            }
-
-            let top = stack[stack.length - 1];
-
+            let top = stack.pop();
             if (
-                (s[i] === ")" && top === "(") ||
-                (s[i] === "]" && top === "[") ||
-                (s[i] === "}" && top === "{")
+                (s[i] === ")" && top !== "(") ||
+                (s[i] === "]" && top !== "[") ||
+                (s[i] === "}" && top !== "{")
+
             ) {
-                stack.pop();
-            } 
-            else {
                 return false;
             }
         }
     }
-
     return stack.length === 0;
 };
-
