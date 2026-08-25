a5a55ee06eee0fb9af021bd4546fc2e279f3b009
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-25 22:02:23 +0530

    Time: 6 ms (65.31%) | Memory: 63.1 MB (80.08%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -2,19 +2,18 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
+var isPalindrome = function (x) {
+    if (x < 0) return false;
 
-    if(x < 0) return false;
-    let xCopy = x;
-    let rem = 0;
     let rev = 0;
+    let rem = 0;
+    let num = x;
 
-
-    while(xCopy > 0){
-    rem = xCopy % 10;
-    rev = (rev * 10) + rem;
-    xCopy = Math.floor(xCopy / 10);
+    while (num > 0) {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = Math.floor(num / 10);
     }
 
-    return x === rev;
+    return rev === x;
 };
