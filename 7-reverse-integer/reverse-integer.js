d81bf8c83dda05edf61a74bab3e9346d8f3e7ded
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-25 10:56:43 +0530

    Time: 46 ms (81.67%) | Memory: 57 MB (25.08%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -5,21 +5,20 @@
 var reverse = function(x) {
     let min = -2147483648;
     let max = 2147483648;
-     
-     let rev = 0;
-     let rem = 0;
-     let xCopy = Math.abs(x);
+    let rev = 0;
+    let rem = 0;
+    let val = Math.abs(x)
 
-    while(xCopy > 0){
-        rem = xCopy%10;
-        rev = (rev * 10) + rem;
-        xCopy =  Math.floor(xCopy/10)
+    while(val > 0){
+        rem = val % 10;
+        rev = rev * 10 + rem;
+        val = Math.floor(val / 10);
     }
-             
-  if( x > 0){
-        return (rev < min || rev > max) ? 0 : rev;
-  }
-  else {
-        return (rev < min || rev > max) ? 0 : -rev;
-  }
+
+    if(x > 0){
+        return (min > rev || max < rev ) ? 0 : rev;
+    }
+    else {
+        return (min > rev || max < rev ) ? 0 : -rev;
+    }
 };
