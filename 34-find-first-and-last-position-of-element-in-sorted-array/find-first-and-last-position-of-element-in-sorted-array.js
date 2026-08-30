7b546299117f35f786ad97067e302107b0d2241a
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-30 22:37:06 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (41.06%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,43 +3,40 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-   
+var searchRange = function (nums, target) {
     let left = 0;
     let right = nums.length - 1;
-    let ans = [-1 , -1];
+    let answer = [-1, -1];
 
-     if (nums.length === 0 ) return ans;
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            answer[0] = mid;
+            right = mid - 1;
+        }
+        else if (target > nums[mid]) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
 
-    while(left <= right ){
-    let mid = Math.floor((left + right)/2);
-    if(nums[mid] === target) {
-        ans[0] = mid;
-        right = mid - 1;
-    }
-    else if(nums[mid] < target){
-     left = mid + 1;
-    }
-    else{
-        right = mid - 1;
-    }
-    }
     left = 0;
     right = nums.length - 1;
 
-
-    while(left <= right){
-     let mid = Math.floor((left + right)/2);
-    if(nums[mid] === target) {
-        ans[1] = mid;
-        left = mid + 1 ;
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            answer[1] = mid;
+            left = mid + 1;
+        }
+        else if (target > nums[mid]) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
     }
-    else if(nums[mid] < target){
-     left = mid + 1;
-    }
-    else{
-        right = mid - 1;
-    }
-    }
-    return ans;
+    return answer;
 };
