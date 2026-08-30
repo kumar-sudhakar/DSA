f1a337ba93acf06f8f77c06f0fb49cd6b2adcaaf
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-08-30 22:01:21 +0530

    Time: 10 ms (30.01%) | Memory: 62.3 MB (55.07%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -2,11 +2,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function (nums) {
-
+var sortedSquares = function(nums) {
+    let n = nums.length;
+    let left = 0;
+    let right = nums.length - 1;
     let answer = [];
-    for (let i = 0; i < nums.length; i++) {
-        answer.push(nums[i] * nums[i]);
+
+    for(let i = n-1; i >= 0 ; i--){
+
+        let leftSquare = nums[left] * nums[left];
+        let rightSquare = nums[right] * nums[right];
+
+        if(leftSquare > rightSquare){
+            answer[i] = leftSquare;
+            left++;
+        }else{
+            answer[i] = rightSquare;
+            right--;
+        }
     }
-    return answer.sort((a, b) => a - b)
+    return answer;
 };
