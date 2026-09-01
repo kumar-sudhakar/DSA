4f299af98b7b63d6a2504a326b7b915c3367d268
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-09-01 22:04:06 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (79.43%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if( n <= 2) {
+        return n
+    }
+
+    let a = 1;
+    let b = 2;
+
+    for(let i = 3 ; i <= n ; i++){
+        let c = a + b;
+        a = b;
+        b = c;
+
+    }
+    return b;
+};
