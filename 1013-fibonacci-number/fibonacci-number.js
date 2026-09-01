f3055d2426f36804f087fbdb82ebf49619bc8768
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-09-01 21:44:59 +0530

    Time: 53 ms (52.97%) | Memory: 53.9 MB (35.97%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n <= 1){
+        return n;
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for(let i = 2 ; i <= n ; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+};
