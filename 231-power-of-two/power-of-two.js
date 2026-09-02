7592cbda8e02afb9b11ee8c6c7dac9778717c171
Sudhakar Kumar <kumarsudhakar213@gmail.com>
2026-09-02 22:01:34 +0530

    Time: 1 ms (36.38%) | Memory: 55.9 MB (52.51%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,10 +3,9 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    
-    if(n == 1) return true;
-
-    if(n%2 != 0 || n <= 0) return false;
-
-    return  isPowerOfTwo(n/2) ;
+    if(n === 1) return true;
+    if(n <= 0 || n % 2 !== 0){
+     return false;
+    }
+    return isPowerOfTwo(n / 2);
 };
